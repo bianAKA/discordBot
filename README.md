1e7e2b0f36fca85a10d082b3a8111edf5c9d17fe
bianAKA <biancarongg@gmail.com>
2025-01-23 15:43:52 +1100

    error fixed; new error comes out: token file isnt generated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # discordBot
 A discordBot that can write bullet points in a google doc by using python
+
+/login: authorise discord bot to have the permission to access
+/password: the password that allows the bot to get the permission
--- cogs/authorization.py
+++ cogs/authorization.py
@@ -17,11 +17,25 @@ class Write(commands.Cog):
         self.bot = bot
         self.creds = None
         self.flow = None
-        self.password = None
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("commands ready")
+    
+    def writingToken(self):
+        print("A")
+        with open("token.json", "w") as token:
+            token_data = {
+                'token': self.creds.token,
+                'refresh_token': self.creds.refresh_token,
+                'token_uri': self.creds.token_uri,
+                'client_id': self.creds.client_id,
+                'client_secret': self.creds.client_secret,
+                'scopes': self.creds.scopes
+            }
+            json.dump(token_data, token)
+        print("B")
+        
     
     @app_commands.command(name="testing", description="see if bot is working")
     async def hi(self, interaction: discord.Interaction):
@@ -29,33 +43,32 @@ class Write(commands.Cog):
 
     @app_commands.command(name="password", description="authorize the program to access the google drive")
     async def password(self, interaction: discord.Interaction, message: str):
-        self.password = message
         await interaction.response.send_message(f"Thanks, we got it!")
         
-        self.creds = self.flow.fetch_token(code=self.password)
-        with open("token.json", "w") as token:
-            token.write(self.creds.to_json())
+        self.creds = self.flow.fetch_token(code=self.message)
+        self.writingToken()
 
     @app_commands.command(name="login", description="get token of google account")
-    async def getCreds(self, interaction: discord.Interaction):
-        await interaction.response.defer(ephemeral=True)
-
+    async def getCreds(self, interaction):
         if os.path.exists("token.json"):
-            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-         
-        if not self.creds or not self.creds.valid:
+            await interaction.response.send_message(f"Hi, {interaction.user.mention}, we already got your token", ephemeral=True)
+            
+            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES) 
+            return
+
+        if not self.creds or not self.creds.valid: 
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
+                self.writingToken()
             else:
                 self.flow = InstalledAppFlow.from_client_secrets_file(
                     "./credentials.json", SCOPES,
                     redirect_uri='urn:ietf:wg:oauth:2.0:oob' 
                 )
 
-                auth_url = flow.authorization_url()
+                auth_url = self.flow.authorization_url()
+            
+                await interaction.response.send_message(f"Hi, {interaction.user.mention}, please go to this link: {auth_url[0]}, and give us the password by using slash command password", ephemeral=True)
                 
-                await interaction.response.send_message(f"Hi, please go to this link: {auth_url[0]}, and give us the password by using slash command password", ephemeral=True)
-                return
-        
 async def setup(bot):
     await bot.add_cog(Write(bot))

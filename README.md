c48632e028a4bfa1efbaa03ab6d18f04bffbc1a7
Bianca Ren <z5417107@student.unsw.edu.au>
2025-01-25 13:07:24 +1100

    it works locally

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A discordBot that can write bullet points in a google doc by using python
 
 /login: authorise discord bot to have the permission to access
-/password: the password that allows the bot to get the permission
+
+
--- cogs/authorization.py
+++ cogs/authorization.py
@@ -21,54 +21,42 @@ class Write(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("commands ready")
-    
-    def writingToken(self):
-        print("A")
-        with open("token.json", "w") as token:
-            token_data = {
-                'token': self.creds.token,
-                'refresh_token': self.creds.refresh_token,
-                'token_uri': self.creds.token_uri,
-                'client_id': self.creds.client_id,
-                'client_secret': self.creds.client_secret,
-                'scopes': self.creds.scopes
-            }
-            json.dump(token_data, token)
-        print("B")
-        
-    
-    @app_commands.command(name="testing", description="see if bot is working")
-    async def hi(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Hi, {interaction.user.mention}")
 
-    @app_commands.command(name="password", description="authorize the program to access the google drive")
-    async def password(self, interaction: discord.Interaction, message: str):
-        await interaction.response.send_message(f"Thanks, we got it!")
-        
-        self.creds = self.flow.fetch_token(code=self.message)
-        self.writingToken()
+    def writingToken(self):
+        with open("token.json", "w") as token:
+            token.write(self.creds.to_json())
 
     @app_commands.command(name="login", description="get token of google account")
-    async def getCreds(self, interaction):
+    async def getCreds(self, interaction: discord.Interaction):
         if os.path.exists("token.json"):
             await interaction.response.send_message(f"Hi, {interaction.user.mention}, we already got your token", ephemeral=True)
             
             self.creds = Credentials.from_authorized_user_file("token.json", SCOPES) 
             return
 
-        if not self.creds or not self.creds.valid: 
+        if not self.creds or not self.creds.valid:
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
-                self.writingToken()
-            else:
-                self.flow = InstalledAppFlow.from_client_secrets_file(
-                    "./credentials.json", SCOPES,
-                    redirect_uri='urn:ietf:wg:oauth:2.0:oob' 
+            else: 
+                await interaction.response.defer(ephemeral=True)
+                await interaction.followup.send(
+                    f"Hi, {interaction.user.mention}, starting authentication process...",
+                    ephemeral=True
                 )
 
-                auth_url = self.flow.authorization_url()
-            
-                await interaction.response.send_message(f"Hi, {interaction.user.mention}, please go to this link: {auth_url[0]}, and give us the password by using slash command password", ephemeral=True)
+                self.flow = InstalledAppFlow.from_client_secrets_file(
+                    "./credentials.json", SCOPES
+                )
+
+                self.creds = self.flow.run_local_server(port=0)
+                
+                await interaction.followup.send(
+                    f"Authentication successful! Credentials saved. We got your token!!!!",
+                    ephemeral=True
+                )
+
+            with open("token.json", "w") as token:
+                token.write(self.creds.to_json())
                 
 async def setup(bot):
     await bot.add_cog(Write(bot))

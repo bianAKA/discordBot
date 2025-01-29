064ae17b9e4ab2551d64956e04b138b4d24ae193
Bianca Ren <z5417107@student.unsw.edu.au>
2025-01-29 18:12:48 +1100

    add mongodb

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # discordBot
-A discordBot that can write bullet points in a google doc by using python
+A discordBot that can write bullet points in a google doc by using python and mongodb
 
 /login: authorise discord bot to have the permission to access
+- mongodb allows a discord user to have multiple email addresses authroised
+- mongodb avoids the multiple of token json files
 
-
+limitations: 
+1) (https://developers.google.com/docs/api/quickstart/python), the account needs to be added as a test user while configure the OAuth consent screen, otherwise it blocks the account.
+2) the URL for authorizing is opened locally
--- cogs/authorization.py
+++ cogs/authorization.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord import app_commands
 import os
 import os.path
+import json
+import pymongo
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -11,6 +13,9 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
+CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+mydb = CLIENT['authoriszations']
+athInfo = mydb.authInformation
 
 class Write(commands.Cog):
     def __init__(self, bot):
@@ -22,21 +27,26 @@ class Write(commands.Cog):
     async def on_ready(self):
         print("commands ready")
 
-    def writingToken(self):
-        with open("token.json", "w") as token:
-            token.write(self.creds.to_json())
-
     @app_commands.command(name="login", description="get token of google account")
-    async def getCreds(self, interaction: discord.Interaction):
-        if os.path.exists("token.json"):
+    async def getCreds(self, interaction: discord.Interaction, emailaddress: str):
+        if athInfo.count_documents({
+            "$and": [
+                {"emailAdress": emailaddress},
+                {"dcUserId": interaction.user.id},
+                {"isAuthorized": True}
+            ]
+        }) != 0:
             await interaction.response.send_message(f"Hi, {interaction.user.mention}, we already got your token", ephemeral=True)
             
-            self.creds = Credentials.from_authorized_user_file("token.json", SCOPES) 
+            tokenInfo = athInfo.find({'emailAdress': emailaddress, 'dcUserId': interaction.user.id}, {_id:False, 'token':True})['token']
+            self.creds = Credentials.from_authorized_user_file(tokenInfo, SCOPES)
             return
-
+        
         if not self.creds or not self.creds.valid:
+            rewritedCreds = False
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
+                rewritedCreds = True
             else: 
                 await interaction.response.defer(ephemeral=True)
                 await interaction.followup.send(
@@ -54,9 +64,21 @@ class Write(commands.Cog):
                     f"Authentication successful! Credentials saved. We got your token!!!!",
                     ephemeral=True
                 )
-
-            with open("token.json", "w") as token:
-                token.write(self.creds.to_json())
+            
+            if rewritedCreds:
+                athInfo.update_one(
+                    {"emailAdress": emailaddress, "dcUserId": interaction.user.id},
+                    {"$set": {"token": self.creds.to_json()}}
+                )
+            else:
+                info = {
+                    'emailAdress': emailaddress,
+                    'dcUserId': interaction.user.id,
+                    'token': json.loads(self.creds.to_json()),
+                    'isAuthorized': True
+                }
+                
+                athInfo.insert_one(info)
                 
 async def setup(bot):
     await bot.add_cog(Write(bot))

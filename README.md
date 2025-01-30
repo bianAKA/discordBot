c276425a60b6b3ae3276e93ea5aefac9f2317b4a
Bianca Ren <z5417107@student.unsw.edu.au>
2025-01-30 14:57:02 +1100

    add end slah command functionality

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ A discordBot that can write bullet points in a google doc by using python and mo
 - it checks if the email address is authorized or not
 - it checks if user already has 'activated' email or not. If user doesn't have then we will activate it
 
+/end: let the bot stops focusing on that google acount
+- it checks if the email address is authorized or not
+- it checks that email address is deactivated or not. If it is activated, we will turn into deactivated
+
 limitations: 
 1) (https://developers.google.com/docs/api/quickstart/python), the account needs to be added as a test user while configure the OAuth consent screen, otherwise it blocks the account.
 2) the URL for authorizing is opened locally
--- cogs/create.py
+++ cogs/create.py
@@ -22,6 +22,28 @@ class AddDocument(commands.Cog):
     async def on_ready(self):
         print("ready to create document/files")
         
+    def isExist(self, email, id):
+        return athInfo.count_documents({
+            "$and": [
+                {"emailAdress": email},
+                {"dcUserId": id}
+            ]
+        }) != 0
+        
+    async def updateActive(self, plsActive):
+        athInfo.update_one(
+            {"emailAdress": email, "dcUserId": interaction.user.id},
+            {"$set": {"isActive": plsActive}}
+        )
+
+        text = ""
+        if plsActive:
+            text = f"Hi, {interaction.user.mention}, you can now edit/add file/folder!"
+        else:
+            text = f"Hi, {interaction.user.mention},it stops now"
+        
+        await interaction.followup.send(text, ephemeral=True)
+        
     @app_commands.command(name="activate", description="tell the bot which account you are using")
     async def active(self, interaction: discord.Interaction, email: str):
         athInfo.create_index([("emailAdress", 1), ("dcUserId", 1)])
@@ -33,12 +55,7 @@ class AddDocument(commands.Cog):
             ephemeral=True
         )
 
-        if athInfo.count_documents({
-            "$and": [
-                {"emailAdress": email},
-                {"dcUserId": interaction.user.id}
-            ]
-        }) != 0:
+        if self.isExist(email, interaction.user.id):
             emailTarget = athInfo.find_one({
                 "isActive": True,
                 "dcUserId": interaction.user.id
@@ -52,15 +69,7 @@ class AddDocument(commands.Cog):
                     f"Hi, {interaction.user.mention}, it seems like one of your account: {email}, is active right now. If it's not the acount you want to use you can use /end to deactivate it."
                 )
             else:
-                athInfo.update_one(
-                    {"emailAdress": email, "dcUserId": interaction.user.id},
-                    {"$set": {"isActive": True}}
-                )
-
-                await interaction.followup.send(
-                    f"Hi, {interaction.user.mention}, you can now edit/add file/folder!",
-                    ephemeral=True
-                )
+                self.updateActive(True)
         else:
             await interaction.followup.send(
                 f"Hi, {interaction.user.mention}, we couldn't find your email address. Please check if the email address is correct, or use /login command",
@@ -71,7 +80,37 @@ class AddDocument(commands.Cog):
 
     @app_commands.command(name="end", description="tell the bot which account you want to end")
     async def deActive(self, interaction: discord.Interaction, email: str):
-        return
+        athInfo.create_index([("emailAdress", 1), ("dcUserId", 1), ("isActive", 1)])
+
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send(
+            f"Hi, {interaction.user.mention}, we are finding your email address",
+            ephemeral=True
+        )
+
+        if self.isExist(email, interaction.user.id):
+            emailTarget = athInfo.find_one({
+                "isActive": True,
+                "dcUserId": interaction.user.id,
+                "emailAdress": email
+            }, {
+                "_id":0,
+                "emailAdress": 1
+            })
+    
+            if emailTarget:
+                self.updateActive(False)
+            else:
+                await interaction.followup.send(
+                    f"Hi, {interaction.user.mention}, it seems like one of your account: {email}, is deactive right now."
+                )
+        else:
+            await interaction.followup.send(
+                f"Hi, {interaction.user.mention}, we couldn't find your email address. Please check if the email address is correct, or use /login command",
+                ephemeral=True
+            )
+        
+        athInfo.drop_indexes()
     
     @app_commands.command(name="create_file", description="create a file")
     async def fileCreate(self, interaction: discord.Interaction):
